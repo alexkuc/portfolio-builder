d63491e45e367d7706950f0cc1daf3967af83b9c
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-04-11 23:37:38 +0300

    Vite config: load host, port and https from env var

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { defineConfig, loadEnv } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// https://vitejs.dev/config/#conditional-config
+export default defineConfig(({ mode }) => {
+  // https://stackoverflow.com/a/66389044
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  const host = process.env.VITE_HOST ?? 'localhost';
+
+  const port = process.env.VITE_PORT ? parseInt(process.env.VITE_PORT) : 5173;
+
+  const https =
+    process.env.VITE_HTTPS_KEY && process.env.VITE_HTTPS_CERT
+      ? {
+          key: readFileSync(
+            resolve(__dirname, 'ssl', process.env.VITE_HTTPS_KEY)
+          ),
+          cert: readFileSync(
+            resolve(__dirname, 'ssl', process.env.VITE_HTTPS_CERT)
+          ),
+        }
+      : true;
+
+  return {
+    plugins: [react()],
+    server: {
+      host,
+      port,
+      https,
+    },
+  };
+});
