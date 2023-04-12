6786e2d5833bda3b28be9824ec7c9ba468c8a1e5
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-04-12 23:18:12 +0300

    Create Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,23 @@
+import classNames from 'classnames';
+import './button.scss';
+
+type Button = JSX.IntrinsicElements['button'];
+
+type Props = {
+  onClick: Button['onClick'];
+  children: React.ReactNode;
+} & Button;
+
+const Button = ({ children, className, ...props }: Props) => {
+  const css = classNames({
+    button: true,
+    [`${className}`]: !!className,
+  });
+  return (
+    <button className={css} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export { Button };
