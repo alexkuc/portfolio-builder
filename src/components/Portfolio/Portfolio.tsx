203bf910808d80bb19479f1d92fb8d33af58eb3f
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-04-13 00:05:12 +0300

    Add components to Sidebar in Portfolio

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,5 +1,10 @@
+import { Button } from '../Button/Button';
+import { Contact } from '../Contact/Contact';
 import { Main } from '../Main/Main';
+import { Photo } from '../Photo/Photo';
+import { Separator } from '../Separator/Separator';
 import { Sidebar } from '../Sidebar/Sidebar';
+import { Text } from '../Text/Text';
 import './portfolio.scss';
 
 type Props = {};
@@ -7,7 +12,24 @@ type Props = {};
 const Portfolio = ({}: Props) => {
   return (
     <div className="portfolio">
-      <Sidebar />
+      <Sidebar>
+        <Photo />
+        <Contact />
+        <Separator />
+        <Text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          pharetra dignissim enim. Mauris mattis molestie nunc eu posuere. Donec
+          hendrerit quis turpis sit amet varius. Sed euismod mi velit, ut
+          condimentum lorem venenatis vitae. Nulla euismod pellentesque mauris
+          id maximus. In dignissim, diam et dignissim faucibus, neque ante
+          venenatis arcu.
+        </Text>
+        <Separator />
+        <Button onClick={() => 'TODO:'}>Work experience</Button>
+        <Button onClick={() => 'TODO:'}>Education</Button>
+        <Button onClick={() => 'TODO:'}>Training</Button>
+        <Button onClick={() => 'TODO:'}>Personal projects</Button>
+      </Sidebar>
       <Main />
     </div>
   );
