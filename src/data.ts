9a308597379850b2bd693ad7e5c1099acb4e3cb0
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-04-14 18:56:03 +0300

    Create dummy data

---
+++ src/data.ts
@@ -0,0 +1,18 @@
+import { Types } from './components/Schemas';
+
+const data: Types['Portfolio'] = {
+  info: {
+    name: 'Jane Doe',
+    intro:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pharetra dignissim enim. Mauris mattis molestie nunc eu posuere. Donec hendrerit quis turpis sit amet varius. Sed euismod mi velit, ut condimentum lorem venenatis vitae. Nulla euismod pellentesque mauris id maximus. In dignissim, diam et dignissim faucibus, neque ante venenatis arcu.',
+    linkedin: 'http://linkedin.com/in/jane.doe/',
+    phone: '+0123456789',
+    email: 'jane.doe@test.test',
+  },
+  jobs: [],
+  education: [],
+  training: [],
+  personal: [],
+};
+
+export default data;
