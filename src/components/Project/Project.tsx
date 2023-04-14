332699feaf832a79e780ed42cda9bc1ad4a12ba6
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-04-14 17:04:30 +0300

    Create Project component

---
+++ src/components/Project/Project.tsx
@@ -0,0 +1,43 @@
+import { Card } from '../Card/Card';
+import { Link } from '../Link/Link';
+import { Schemas, Types } from '../Schemas';
+import { Separator } from '../Separator/Separator';
+import './project.scss';
+
+const Project = (props: Types['Project']) => {
+  const { name, summary, points, links } = Schemas.Project.parse(props);
+
+  const header = (
+    <>
+      <p className="project__title">{name}</p>
+      <Separator />
+    </>
+  );
+
+  const children = (
+    <>
+      <div className="project">
+        <p className="project__summary">{summary}</p>
+        {points.length > 0 && (
+          <ul className="project__points">
+            {points.map((p) => (
+              <li className="project__point">{p}</li>
+            ))}
+          </ul>
+        )}
+        <Separator />
+        {links.length > 0 && (
+          <div className="project__links">
+            {links.map((l) => (
+              <Link href={l.url}>{l.name}</Link>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
+
+  return <Card header={header}>{children}</Card>;
+};
+
+export { Project };
