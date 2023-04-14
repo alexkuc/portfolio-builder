e72495aebe599efd134e3c9d6eed0bfde5ca97cd
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-04-14 17:32:40 +0300

    Create Course component

---
+++ src/components/Course/Course.tsx
@@ -0,0 +1,38 @@
+import { v4 } from 'uuid';
+import { Card } from '../Card/Card';
+import { Link } from '../Link/Link';
+import { Schemas, Types } from '../Schemas';
+import { Separator } from '../Separator/Separator';
+import './course.scss';
+
+const Course = (props: Types['Course']) => {
+  const { name, summary, links } = Schemas.Course.parse(props);
+
+  const header = <p className="course__title">{name}</p>;
+
+  const children = (
+    <>
+      <p className="project__summary">{summary}</p>
+      {links.length > 0 && (
+        <>
+          <Separator />
+          <ul className="project__links">
+            {links.map((l) => (
+              <Link key={v4()} href={l.url}>
+                {l.name}
+              </Link>
+            ))}
+          </ul>
+        </>
+      )}
+    </>
+  );
+
+  return (
+    <div className="course">
+      <Card header={header}>{children}</Card>
+    </div>
+  );
+};
+
+export { Course };
