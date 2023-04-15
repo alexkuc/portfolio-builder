56f0be9610fddd625c6312a7ab2dea792509b659
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-04-15 19:19:51 +0300

    Paragraph to support pass-through props

--- src/components/Paragraph/Paragraph.tsx
+++ src/components/Paragraph/Paragraph.tsx
@@ -1,12 +1,21 @@
+import classNames from 'classnames';
 import React from 'react';
 import './paragraph.scss';
 
 type Props = {
   children: React.ReactNode;
-};
+} & JSX.IntrinsicElements['p'];
 
-const Paragraph = ({ children }: Props) => {
-  return <p className="paragraph">{children}</p>;
+const Paragraph = ({ children, className, ...props }: Props) => {
+  const css = classNames({
+    paragraph: true,
+    [`${className}`]: !!className,
+  });
+  return (
+    <p className={css} {...props}>
+      {children}
+    </p>
+  );
 };
 
 export { Paragraph };
