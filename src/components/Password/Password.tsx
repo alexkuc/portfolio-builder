128dbc59977cf059fde325e8b4684336e11e0e8a
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-04-24 01:33:38 +0300

    Create simple Password component

---
+++ src/components/Password/Password.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from 'react';
+import './password.scss';
+
+type Props = {
+  children: React.ReactNode | React.ReactNode[];
+  hash: string;
+};
+
+type State = string | null;
+
+const Password = ({ children, hash }: Props) => {
+  const [attempt, setAttempt] = useState<State>(null);
+  const [unlocked, setUnlocked] = useState(false);
+
+  const promptPass = () => {
+    setAttempt(window.prompt('Please enter password'));
+  };
+
+  useEffect(() => {
+    promptPass();
+  }, []);
+
+  useEffect(() => {
+    if (!attempt) return;
+    digestMessage(attempt).then((h) => {
+      if (h === hash) setUnlocked(true);
+      if (h !== hash) promptPass();
+    });
+  }, [attempt]);
+
+  return <>{unlocked && children}</>;
+};
+
+type Algorithm = 'SHA-1' | 'SHA-256' | 'SHA-384' | 'SHA-512';
+
+// https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#converting_a_digest_to_a_hex_string
+async function digestMessage(
+  message: string,
+  algorith?: Algorithm | undefined
+) {
+  if (!algorith) algorith = 'SHA-512';
+  // encode as (utf-8) Uint8Array
+  const msgUint8 = new TextEncoder().encode(message);
+  // hash the message
+  const hashBuffer = await crypto.subtle.digest(algorith, msgUint8);
+  // convert buffer to byte array
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, '0'))
+    .join(''); // convert bytes to hex string
+  return hashHex;
+}
+
+export { Password };
