b03fde2eb15ac6c8b1167fa8329246413c053591
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-04-30 19:39:42 +0300

    Add phone to data.ts

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -14,7 +14,7 @@ const data: Types['Portfolio'] = {
     intro:
       'My name is Alexander and I am a full-stack WordPress developer. Technology have always been my passion even though my higher education degree is closer related to management. In my mind, I always knew I would end up working in IT industry. My career path has started as a WordPress developer. My capabilities range from setting up a pre-made theme to writing a fully custom solution. My short term goal is to become a frontend developer with a primary specialty in React. In the last two years, I have written plenty of TypeScript-based React widgets. My long term goal is to become a CTO or hold a similar senior management position.',
     linkedin: 'https://linkedin.com/in/alexkuc',
-    // phone: '', // TODO:
+    phone: '+441313990232',
     email: 'alexander.kucheryuk@icloud.com',
   },
   jobs: [
