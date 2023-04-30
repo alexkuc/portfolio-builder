35a3bd3e4aaf54151dbb793896868e17ab4c4a60
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-04-30 20:36:35 +0300

    Remove photo

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -3,14 +3,12 @@ import { Types } from '../components/Schemas';
 import { Education } from './Education';
 import { Projects } from './Projects';
 import { Jobs } from './jobs/Jobs';
-import photo from './photo.jpg';
 import { Training } from './training/Training';
 
 const data: Types['Portfolio'] = {
   info: {
     name: 'Alexander Kucheryuk',
     // https://github.com/colinhacks/zod/issues/2025#issuecomment-1445451748
-    photo: new URL(photo, window.location.href).href,
     intro:
       'My name is Alexander and I am a full-stack WordPress developer. Technology have always been my passion even though my higher education degree is closer related to management. In my mind, I always knew I would end up working in IT industry. My career path has started as a WordPress developer. My capabilities range from setting up a pre-made theme to writing a fully custom solution. My short term goal is to become a frontend developer with a primary specialty in React. In the last two years, I have written plenty of TypeScript-based React widgets. My long term goal is to become a CTO or hold a similar senior management position.',
     about: {
