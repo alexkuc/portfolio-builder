2573e035cee7c6846c872661a531ebcf70cd30bf
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-04-30 22:14:47 +0300

    Re-order IronFX.ts in descending order i.e. newest first

--- src/data/jobs/IronFX.tsx
+++ src/data/jobs/IronFX.tsx
@@ -1,6 +1,77 @@
 import { Types } from '../../components/Schemas';
 import { date } from '../data';
 
+const WebTechLead: Types['Job'] = {
+  position: 'Web Tech Lead',
+  company: 'IronFX',
+  start: date('01.23'),
+  summary:
+    'As a Web Tech Lead, it is my responsibility to drive technical architure and day-to-day technical decisions. These activities range from mandane tasks like choosing a library to developing a website architecture and finding appropriate technical solutions to deliver given technical requirements. To date, I have been working almost my entire on the Academy project.',
+  projects: [
+    {
+      name: 'LMS project',
+      points: [],
+      summary: [
+        <>
+          The nature of this project is to provide online learning coursesusing
+          LMS or Learning Management System. I am the sole person responsible
+          for implementing business logic into actual code and bridging it with
+          internal systems.
+        </>,
+        <>
+          I was the key driver behind the project timeline and estimation. I
+          have broken down the initially given requirements into milestones and
+          features. The approximate time for each was gauged using T-shirt sizes
+          and my colleagues, who participated in the planning poker.
+        </>,
+        <>
+          Login and registration forms were implemented using React, TypeScript,
+          Formik and custom WP REST endpoint. This allowed to create isomorphic
+          code between backend and frontend which was necessary for SugarCRM
+          integration. It required additional email verification step which was
+          also implemented using custom code.
+        </>,
+        <>
+          Google Tag Manager integration was done using SSE or Server-sent
+          events as 3/4 of the events was coming from WordPress side. SSE
+          implementation features a simple concurrency lock and grace lifecycle
+          for events which prevents duplicate events.
+        </>,
+        <>
+          I have overhauled the security of the implementation but details are
+          omitted to prevent accidentally exposing them to public.
+        </>,
+      ],
+      links: [
+        {
+          name: 'React',
+          href: 'https://react.dev',
+        },
+        {
+          name: 'TypeScript',
+          href: 'https://www.typescriptlang.org',
+        },
+        {
+          name: 'Formik',
+          href: 'https://www.npmjs.com/package/formik',
+        },
+        {
+          name: 'MJML',
+          href: 'https://mjml.io',
+        },
+        {
+          name: 'JWT',
+          href: 'https://jwt.io',
+        },
+        {
+          name: 'SSE',
+          href: 'https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events',
+        },
+      ],
+    },
+  ],
+};
+
 const Developer: Types['Job'] = {
   position: 'WordPress Developer',
   company: 'IronFX',
@@ -202,77 +273,6 @@ const Developer: Types['Job'] = {
         {
           name: 'Sass',
           href: 'https://sass-lang.com',
-        },
-      ],
-    },
-  ],
-};
-
-const WebTechLead: Types['Job'] = {
-  position: 'Web Tech Lead',
-  company: 'IronFX',
-  start: date('01.23'),
-  summary:
-    'As a Web Tech Lead, it is my responsibility to drive technical architure and day-to-day technical decisions. These activities range from mandane tasks like choosing a library to developing a website architecture and finding appropriate technical solutions to deliver given technical requirements. To date, I have been working almost my entire on the Academy project.',
-  projects: [
-    {
-      name: 'LMS project',
-      points: [],
-      summary: [
-        <>
-          The nature of this project is to provide online learning coursesusing
-          LMS or Learning Management System. I am the sole person responsible
-          for implementing business logic into actual code and bridging it with
-          internal systems.
-        </>,
-        <>
-          I was the key driver behind the project timeline and estimation. I
-          have broken down the initially given requirements into milestones and
-          features. The approximate time for each was gauged using T-shirt sizes
-          and my colleagues, who participated in the planning poker.
-        </>,
-        <>
-          Login and registration forms were implemented using React, TypeScript,
-          Formik and custom WP REST endpoint. This allowed to create isomorphic
-          code between backend and frontend which was necessary for SugarCRM
-          integration. It required additional email verification step which was
-          also implemented using custom code.
-        </>,
-        <>
-          Google Tag Manager integration was done using SSE or Server-sent
-          events as 3/4 of the events was coming from WordPress side. SSE
-          implementation features a simple concurrency lock and grace lifecycle
-          for events which prevents duplicate events.
-        </>,
-        <>
-          I have overhauled the security of the implementation but details are
-          omitted to prevent accidentally exposing them to public.
-        </>,
-      ],
-      links: [
-        {
-          name: 'React',
-          href: 'https://react.dev',
-        },
-        {
-          name: 'TypeScript',
-          href: 'https://www.typescriptlang.org',
-        },
-        {
-          name: 'Formik',
-          href: 'https://www.npmjs.com/package/formik',
-        },
-        {
-          name: 'MJML',
-          href: 'https://mjml.io',
-        },
-        {
-          name: 'JWT',
-          href: 'https://jwt.io',
-        },
-        {
-          name: 'SSE',
-          href: 'https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events',
         },
       ],
     },
