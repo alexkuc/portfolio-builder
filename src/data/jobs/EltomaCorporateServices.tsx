f47377027dc0f37d0003ea728c21e78729527324
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2023-04-30 23:26:33 +0300

    Update text in EltomaCorporateServices.tsx

--- src/data/jobs/EltomaCorporateServices.tsx
+++ src/data/jobs/EltomaCorporateServices.tsx
@@ -5,44 +5,44 @@ const ITTeamLeader: Types['Job'] = {
   position: 'IT Team Leader',
   company: 'Eltoma Corporate Services',
   summary:
-    "As an IT Team Leader, I had 3 employees and 2 contractors reporting to me directly. The employees were responsible for IT support while contractors were working on the company's websites. Aside from my technical obligations, I was also actively invovled with HR roles of the IT department.",
+    "I was the IT Team Leader, and I was directly responsible for 3 employees and 2 contractors. The company's websites were being developed by contractors, and the staff was in charge of providing IT assistance. Along with my technical responsibilities, I was actively involved in the IT department's HR duties.",
   start: date('01.18'),
   end: date('01.20'),
   projects: [
     {
       name: 'IT Department restructure',
       summary:
-        'I have overhauled department by converting it into Kanban-style management where Trello was the primary tool for keeping track of tasks. Additionally, I have introduced code-based budgetting & expenses tracking. This gave transparency into expenses & assets of the department.Finally, I have reduced capital expenditure of the department by switching to OpEx model instead of CapEx. Operations-wise, I have introduced checklists and procedures by means of Trello templates for routine tasks such as onboarding a new employee or provisioning a new hardware for them. This ensured consistent and high quality of work avoid "Oops, I forgot..." moments from the IT department.',
+        "By switching the department over to Kanban-style management, where Trello served as the main task management tool, I completely redesigned the division. Aside from that, I have added code-based budgeting and expense tracking. This made the department's costs and assets transparent. Finally, by using the OpEx model rather than the CapEx model, I have decreased the department's capital expenditure. For operational tasks like onboarding a new employee or provisioning new equipment for them, I have implemented checklists and procedures using Trello templates. This prevented the IT staff from having any 'Oops, I forgot...' moments and assured consistency and excellent quality of work.",
     },
     {
       name: 'Documentation and internal audit',
       summary:
-        "I have performed an internal audit and have created a comprehensive internal documentation of the IT systems. As the company was spread between 6 geo-remote offices in Russia, Ukraine, Singapore, and UK, having an up-to-date documentation was paramount. The documentation consisted of network map, purchased licenses and inventory. The network map was created in LucidChart and provided both, a top level topology overview as well as fine details like port numbers. Information on purchases licenses scattered all over the places so I have diligently assembled everything and stored it in Confluence. The information included invoices, license numbers, activation limit and the machine where it was activated as well as vendor's contact information. Invetory included information about on-premise servers, office laptops and desktops. Information included network MACs, serial numbers, and related.",
+        'I created significant internal documentation of the IT systems while conducting an internal audit. The company was spread out among six geo-remote offices in Russia, Ukraine, Singapore, and the UK, so having up-to-date documentation was essential. The documentation included an inventory, a network map, and software licenses that had been purchased. The network map included both a high-level topology overview and specific information like port numbers. Since information on purchased licenses was scattered all over the place, I painstakingly put everything together. The data included contact details for the vendor, invoices, license numbers, the computer where it was activated, and the activation limit. Information on office desktops, laptops, and on-premise servers was included in the inventory.',
     },
     {
       name: 'On-premise servers migration',
       summary:
-        'As the company was migrating on-premise servers to MSP, I have assisted with server application migration as I had the necessary domain knowledge on that. This migration was part of CapEx to OpEx migration.',
+        'I helped with the server application migration as the business migrated its on-premise servers to a Managed Services Provider (MSP), as I had the requisite domain expertise in that area. The CapEx to OpEx conversion was the primary driver behind this move.',
     },
     {
       name: 'Centralized password management',
       summary:
-        'I have migrated the IT department from Keepass to PassWork. Not only it made much easier to share specific passwords with vendors but it also gave transparency via audit logs and revision history.',
+        'I switched the IT division over to PassWork from Keepass. Sharing specific passwords with vendors became a lot simpler thanks to this, and it also provided transparency thanks to audit logs and revision history.',
     },
     {
       name: 'MODX to WordPress migration',
       summary:
-        "I was actively involved with migrating the main company's website from MODX to WordPress. To avoid any SEO-related penalties, I was very careful to preserve existing url structure, tags and related as it is during the migration.",
+        'I played a significant role in the conversion of the main business website from MODX to WordPress. I took great care to keep the current URL structure, tags, and related information the same throughout the migration in order to prevent any SEO-related issues.',
     },
     {
       name: 'Domain tracking and dnmin',
       summary:
-        'As a part of business operations, each client required a simple website that was hosted on their on domain. Given that there were dozens of domains that had different expiry dates as well as renewal procedures, a centralized system for keeping track of them was necessary. I have consolidated and centralized this process using FOSS or Free Open Source Software called dnmin.',
+        'To incorporate their firm, each client required a simple website that was hosted on their own domain. Because there were numerous domains with various expiration dates and renewal procedures, a unified method for managing domains was necessary. I have employed dnmin, a free open source program, to centralize and streamline this procedure.',
     },
     {
       name: 'Sage 50c Accounts and SPS training',
       summary:
-        "For its operations, the company was using Sage 50c Accounts for book keeping and Sage Practice Solutions for overall management of work. This in turned required me to become a domain expert on these 2 pieces of software in order to provide daily Q&A as well as clear communicate with the vendor's support line. Of course, it has helped that I do have a basic knowledge of debit and credit accounting model.",
+        "The business used Sage Practice Solutions for general work management and Sage 50c Accounts for bookkeeping in its operations. As a result, I had to become an expert on these two pieces of software in order to answer daily questions and have clear conversations with the vendor's support team. Of course, the fact that I am familiar with the debit and credit accounting concepts has been helpful.",
     },
   ],
 };
@@ -51,29 +51,29 @@ const JuniorSystemAdministrator: Types['Job'] = {
   position: 'Junior System Administrator',
   company: 'Eltoma Corporate Services',
   summary:
-    'This is my very first job in the IT industry. As the title says, I was a junior sysadmin slash tech support. My work ranged from simple client troubleshooting to deploying Cisco ASA VPN between 2 geo-remote sites.',
+    'My first position in the IT sector is this one. I was a junior system administrator and tech support specialist, as the title indicates. My tasks varied from straightforward client troubleshooting to setting up a Cisco ASA VPN between two geographically distant sites.',
   start: date('06.11'),
   end: date('12.13'),
   projects: [
     {
       name: 'Cisco ASA VPN',
       summary:
-        'I have set up a site-to-site vpn tunnel between two Cisco ASA 5505 where one was located in Cyprus and the other in Singapore.',
+        'A site-to-site VPN tunnel was established between two Cisco ASA 5505s, one of which was situated in Singapore and the other in Cyprus.',
     },
     {
       name: 'Windows Active Directory',
       summary:
-        'In order to install Sage Practice Solution software, I had to migrate the entire network of about 25 computers from workgroup to Active Directory. It was running Windows Server 2008 at the time.',
+        'I had to switch the complete network of roughly 25 PCs from workgroup to Active Directory in order to install Sage Practice Solution software. At the time, Windows Server 2008 was being used.',
     },
     {
       name: 'Server overhaul',
       summary:
-        'I have purchased, assembled and configured a tower server that have replaced previously running Linksys NAS. This project goes hand-in-hand with Windows Active Directory project.',
+        'I purchased, assembled, and installed a tower server to swap out the previously functional Linksys NAS. This is closely related to the Windows Active Directory project.',
     },
     {
       name: 'Kaspersky Endpoint Security',
       summary:
-        'I have purchased, installed and configured Kaspersky Endpoint Security to replace previously running home version of Kaspersky software. Unlike home edition, KES is an enterprise software that allowed central control of policies. For exampke, keeping consistent configuration, vary internet access according to AD OUs and receiving reports of caught malware.',
+        'To replace the earlier, home edition version, I have now bought, installed, and configured KES, or Kaspersky Endpoint Security. KES was an enterprise program that allowed for centralized policy control. For instance, maintaining consistent settings, adjusting internet access in accordance with AD OUs, and receiving reports of malware that has been discovered',
     },
   ],
 };
